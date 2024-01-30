64f36193437fad292b4726b15f2782fe1d244e6a
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2024-01-30 10:35:42 -0800

    Add frames validation

--- pages/api/vote.ts
+++ pages/api/vote.ts
@@ -1,74 +1,104 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import {Poll} from "@/app/types";
-import {kv} from "@vercel/kv";
-import {getSSLHubRpcClient, Message} from "@farcaster/hub-nodejs";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Poll } from "@/app/types";
+import { kv } from "@vercel/kv";
+import { validateFramesPost } from "@xmtp/frames-validator";
+import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
 
-const HUB_URL = process.env['HUB_URL'] || "nemes.farcaster.xyz:2283"
+const HUB_URL = process.env["HUB_URL"] || "nemes.farcaster.xyz:2283";
 const client = getSSLHubRpcClient(HUB_URL);
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        // Process the vote
-        // For example, let's assume you receive an option in the body
-        try {
-            const pollId = req.query['id']
-            const results = req.query['results'] === 'true'
-            let voted = req.query['voted'] === 'true'
-            if (!pollId) {
-                return res.status(400).send('Missing poll ID');
-            }
+async function validateFarcasterMessage(
+  frameMessageBytes: Buffer
+): Promise<string> {
+  const frameMessage = Message.decode(frameMessageBytes);
+  const result = await client.validateMessage(frameMessage);
+  if (result.isOk() && result.value.valid) {
+    const fid = result.value.message?.data?.fid?.toString();
+    if (fid) {
+      return fid;
+    }
+  }
+  throw new Error("Failed to validate message");
+}
 
-            let validatedMessage : Message | undefined = undefined;
-            try {
-                const frameMessage = Message.decode(Buffer.from(req.body?.trustedData?.messageBytes || '', 'hex'));
-                const result = await client.validateMessage(frameMessage);
-                if (result.isOk() && result.value.valid) {
-                    validatedMessage = result.value.message;
-                }
-            } catch (e)  {
-                return res.status(400).send(`Failed to validate message: ${e}`);
-            }
+async function validateMessage(body: any): Promise<string> {
+  if (body?.untrustedData?.walletAddress) {
+    const data = await validateFramesPost(body);
+    return data?.verifiedWalletAddress;
+  }
+  return validateFarcasterMessage(
+    Buffer.from(body?.trustedData?.messageBytes || "", "hex")
+  );
+}
 
-            // const buttonId = validatedMessage?.data?.frameActionBody?.buttonIndex || 0;
-            // const fid = validatedMessage?.data?.fid || 0;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    // Process the vote
+    // For example, let's assume you receive an option in the body
+    try {
+      const pollId = req.query["id"];
+      const results = req.query["results"] === "true";
+      let voted = req.query["voted"] === "true";
+      if (!pollId) {
+        return res.status(400).send("Missing poll ID");
+      }
 
-            // Use untrusted data for testing
-            const buttonId = req.body?.untrustedData?.buttonIndex || 0;
-            const fid = req.body?.untrustedData?.fid || 0;
+      let identifier: string;
+      try {
+        identifier = await validateMessage(req.body);
+      } catch (e) {
+        return res.status(400).send(`Failed to validate message: ${e}`);
+      }
 
-            const voteExists = await kv.sismember(`poll:${pollId}:voted`, fid)
-            voted = voted || !!voteExists
+      // const buttonId = validatedMessage?.data?.frameActionBody?.buttonIndex || 0;
+      // const fid = validatedMessage?.data?.fid || 0;
 
-            // Clicked create poll
-            if ((voted || results) && buttonId === 2) {
-                return res.status(302).setHeader('Location', `${process.env['HOST']}`).send('Redirecting to create poll');
-            }
+      // Use untrusted data for testing
+      const buttonId = req.body?.untrustedData?.buttonIndex || 0;
 
-            if (fid > 0 && buttonId > 0 && buttonId < 5 && !results && !voted) {
-                let multi = kv.multi();
-                multi.hincrby(`poll:${pollId}`, `votes${buttonId}`, 1);
-                multi.sadd(`poll:${pollId}:voted`, fid);
-                await multi.exec();
-            }
+      const voteExists = await kv.sismember(`poll:${pollId}:voted`, identifier);
+      voted = voted || !!voteExists;
 
-            let poll: Poll | null = await kv.hgetall(`poll:${pollId}`);
+      // Clicked create poll
+      if ((voted || results) && buttonId === 2) {
+        return res
+          .status(302)
+          .setHeader("Location", `${process.env["HOST"]}`)
+          .send("Redirecting to create poll");
+      }
 
-            if (!poll) {
-                return res.status(400).send('Missing poll ID');
-            }
-            const imageUrl = `${process.env['HOST']}/api/image?id=${poll.id}&results=${results ? 'false': 'true'}&date=${Date.now()}${ fid > 0 ? `&fid=${fid}` : '' }`;
-            let button1Text = "View Results";
-            if (!voted && !results) {
-                button1Text = "Back"
-            } else if (voted && !results) {
-                button1Text = "Already Voted"
-            } else if (voted && results) {
-                button1Text = "View Results"
-            }
+      if (identifier && buttonId > 0 && buttonId < 5 && !results && !voted) {
+        let multi = kv.multi();
+        multi.hincrby(`poll:${pollId}`, `votes${buttonId}`, 1);
+        multi.sadd(`poll:${pollId}:voted`, identifier);
+        await multi.exec();
+      }
 
-            // Return an HTML response
-            res.setHeader('Content-Type', 'text/html');
-            res.status(200).send(`
+      let poll: Poll | null = await kv.hgetall(`poll:${pollId}`);
+
+      if (!poll) {
+        return res.status(400).send("Missing poll ID");
+      }
+      const imageUrl = `${process.env["HOST"]}/api/image?id=${
+        poll.id
+      }&results=${results ? "false" : "true"}&date=${Date.now()}${
+        identifier ? `&fid=${identifier}` : ""
+      }`;
+      let button1Text = "View Results";
+      if (!voted && !results) {
+        button1Text = "Back";
+      } else if (voted && !results) {
+        button1Text = "Already Voted";
+      } else if (voted && results) {
+        button1Text = "View Results";
+      }
+
+      // Return an HTML response
+      res.setHeader("Content-Type", "text/html");
+      res.status(200).send(`
       <!DOCTYPE html>
       <html>
         <head>
@@ -77,23 +107,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           <meta property="og:image" content="${imageUrl}">
           <meta name="fc:frame" content="vNext">
           <meta name="fc:frame:image" content="${imageUrl}">
-          <meta name="fc:frame:post_url" content="${process.env['HOST']}/api/vote?id=${poll.id}&voted=true&results=${results ? 'false' : 'true'}">
+          <meta name="fc:frame:post_url" content="${
+            process.env["HOST"]
+          }/api/vote?id=${poll.id}&voted=true&results=${
+        results ? "false" : "true"
+      }">
           <meta name="fc:frame:button:1" content="${button1Text}">
           <meta name="fc:frame:button:2" content="Create your poll">
           <meta name="fc:frame:button:2:action" content="post_redirect">
         </head>
         <body>
-          <p>${ results || voted ? `You have already voted. You clicked ${buttonId}` : `Your vote for ${buttonId} has been recorded for fid ${fid}.` }</p>
+          <p>${
+            results || voted
+              ? `You have already voted. You clicked ${buttonId}`
+              : `Your vote for ${buttonId} has been recorded for ${identifier}.`
+          }</p>
         </body>
       </html>
     `);
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Error generating image');
-        }
-    } else {
-        // Handle any non-POST requests
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error generating image");
     }
+  } else {
+    // Handle any non-POST requests
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
